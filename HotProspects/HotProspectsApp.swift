15de9f7b2202334c1ac993324a6da3b6be4629ad
NhanHC-dev <nhanhc.dev@gmail.com>
2022-06-08 10:55:49 +0700

    Add files via upload

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Hoàng Chí Nhân on 18/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
